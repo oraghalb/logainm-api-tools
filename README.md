b663ce1e5fd7f42bc4627234542d3459c426e040
Brian Ó Raghallaigh <brian.oraghallaigh@dcu.ie>
2022-04-25 16:22:38 +0100

    Script 1 and README

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# logainm-api-tools
+# Logainm API Tools
+
+A collection of example Python scripts to guide users as to how they might retrieve, parse and use data from the Logainm API ([docs.gaois.ie](https://docs.gaois.ie/en/data/getting-started)).
+
+## Script 1: logainm_api_results_to_table.py
+
+This script gets a list of places of a specified type from a specified county in Ireland from the Logainm API and prints place metadata (one place per line) to a TSV formatted file with the following columns:
+
+> logainm_id | lat | lon | county | barony | parish | cats | names_en | names_ga
+
+For example, for townlands (BF) in Carlow (100004), this would be the first five lines of output (vertical bars are inserted here for clarity):
+
+> 3000 | 52.8268601272065 | -6.58634818810364 | 100004 Carlow | 1 Rathvilly | 1411069 Clonmore | townland | Ballaghaclay | Bealach an tSléibhe
+> 3001 | 52.8388492391207 | -6.53371364645032 | 100004 Carlow | 1 Rathvilly | 1411069 Clonmore | townland | Ballinagilky | Baile na Giolcaí
+> 3002 | 52.8365069012364 | -6.60636780935882 | 100004 Carlow | 1 Rathvilly | 1411069 Clonmore | townland | Ballyduff | An Baile Dubh
+> 3003 | 52.8258526764742 | -6.60526952627927 | 100004 Carlow | 1 Rathvilly | 1411069 Clonmore | townland | Ballynakill | Baile na Coille
+> 3004 | 52.8235812668175 | -6.54264046126135 | 100004 Carlow | 1 Rathvilly | 1411069 Clonmore | townland | Bellmount | Bellmount
+> ...
+
+This script can easily be modified to produce different output.
+
+## Script 2: lorem ipsum
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
---
+++ logainm_api_results_to_table.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+
+"""
+This module gets a list of places of a specified type from a specified county in Ireland
+
+from the Logainm API (See: docs.gaois.ie) and prints place metadata to a TSV formatted file.
+"""
+
+import json
+import os
+import requests
+
+class Place:
+  def __init__(self, logainm_id, lat, lon, county, barony, parish, cats, names_en, names_ga):
+    self.logainm_id = logainm_id
+    self.lat = lat
+    self.lon = lon
+    self.county = county
+    self.barony = barony
+    self.parish = parish
+    self.cats = cats
+    self.names_en = names_en
+    self.names_ga = names_ga
+
+def parse_place(place_data):
+
+    # Get place ID:
+    logainm_id = place_data['id']
+    
+    # Get place coordinates:
+    lat = ''
+    lon = ''
+    if 'geography' in place_data:
+        geography = place_data['geography']
+        coordinates = geography['coordinates'] # list of coordinates
+        if coordinates:
+            lat = coordinates[0]['latitude']
+            lon = coordinates[0]['longitude']
+
+    # Get place parents (county, barony and parish parents):
+    parents = place_data['includedIn'] # list of dicts
+    county = ''
+    barony = ''
+    parish = ''
+    for parent in parents:
+        if parent['category']['id'] == 'CON':
+            county = str(parent['id']) + ' ' + parent['nameEN']
+        if parent['category']['id'] == 'BAR':
+            barony = str(parent['id']) + ' ' + parent['nameEN']
+        if parent['category']['id'] == 'PAR':
+            parish = str(parent['id']) + ' ' + parent['nameEN']
+
+    # Get place categories:
+    categories = place_data['categories'] # list of dicts
+    cats = []
+    for category in categories:
+        cats.append(category['nameEN'])
+
+    # Get place names:
+    placenames = place_data['placenames'] # list of dicts
+    names_en = []
+    names_ga = []
+    for placename in placenames:
+        if placename['language'] == 'en':
+            names_en.append(placename['wording'])
+        if placename['language'] == 'ga':
+            names_ga.append(placename['wording'])
+
+    print(str(logainm_id))
+    p = Place(str(logainm_id), str(lat), str(lon), county, barony, parish, ', '.join(cats), ', '.join(names_en), ', '.join(names_ga))
+    return p
+
+place_id = '100004' # EDIT (i.e. 100000 to 100031)
+category_id = 'BF' # EDIT (e.g. CON, BAR, PAR, BF, TR, B)
+api_key = '' # Get key here: https://www.gaois.ie/en/technology/developers/registration/
+
+# Run API query and cache (i.e. save to file), if not already cached:
+if not os.path.isfile('logainm_'+place_id+'_'+category_id+'.json'):
+    json_payload = requests.get('https://www.logainm.ie/api/v1.0/?PlaceID='+place_id+'&CategoryID='+category_id+'&apiKey='+api_key).text # See: docs.gaois.ie
+    with open('logainm_'+place_id+'_'+category_id+'.json', 'w', encoding='utf-8') as f:
+        f.write(json_payload)
+
+# Read cached data:
+with open('logainm_'+place_id+'_'+category_id+'.json', 'r', encoding='utf-8') as fi:
+    json_payload = fi.read()
+
+# Convert from JSON to Python (Object to dict, Array to list, etc.):
+results_data = json.loads(json_payload) # dict
+results = results_data['results'] # list
+
+fo = open('logainm_'+place_id+'_'+category_id+'.tsv', 'w', encoding='utf-8') # output file (TSV can be pasted into Excel)
+fo.write('LOGAINM_ID\tLAT\tLON\tCOUNTY\tBARONY\tPARISH\tCATS\tNAMES_EN\tNAMES_GA\n') # write header
+
+# Write places to TSV file, one per line:
+for place in results:
+    p = parse_place(place)
+    fo.write(p.logainm_id + '\t' + p.lat + '\t' + p.lon + '\t' + p.county + '\t' + p.barony + '\t' + p.parish + '\t' + p.cats + '\t' + p.names_en + '\t' + p.names_ga + '\n')
+
+fo.close()
